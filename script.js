094fcd26767b6995efe49f49df52261629b45f49
Daniel Hevesy <daniel.hevesy-szettyan@student.tugraz.at>
2020-01-25 12:02:31 +0100

    made new page

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
+var redirectBtn = document.querySelector('.redirect-button');
+
+redirectBtn.addEventListener('click', () => {
+    location.href='/makeJoint.html';
+});
 
 convertBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
